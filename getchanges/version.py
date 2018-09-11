9c1bb62dba8693c6788937f374266cfdb2efe2a3
Kevin James <KevinJames@thekev.in>
2018-09-11 00:48:45 -0700

    fix(cli): allow usage without explicit installation

--- getchanges/version.py
+++ getchanges/version.py
@@ -1,4 +1,7 @@
 import pkg_resources
 
 
-__version__ = pkg_resources.get_distribution('getchanges').version
+try:
+    __version__ = pkg_resources.get_distribution('getchanges').version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'dev'
