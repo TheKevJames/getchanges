3426438e23c27f93adc08b295d654d933ff06d5a
Kevin James <KevinJames@thekev.in>
2018-09-11 19:22:52 -0700

    feat(compatibility): drop support for Python 3.5

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
